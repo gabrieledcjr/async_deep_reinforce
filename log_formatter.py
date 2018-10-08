6a2e8c48b6f5ca65573747e036268622a448b5a7
Gabriel de la Cruz <gabrieledcjr@gmail.com>
2018-10-08 00:42:32 -0700

    create common package, create single demo collection

--- log_formatter.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-import logging
-import re
-
-regex = re.compile(r"\x1b(\[.*?[@-~]|\].*?(\x07|\x1b\\))", re.UNICODE)
-class LogFormatter(logging.Formatter):
-    def format(self,record):
-        msg = super(LogFormatter, self).format(record)
-        return regex.sub("", msg)
